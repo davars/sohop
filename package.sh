7372f2e32957e117f529978b1299f2596c34998f
Dave Jack <davars@gmail.com>
2019-03-27 09:39:12 -0400

    Bump go version

--- package.sh
+++ package.sh
@@ -8,6 +8,6 @@ root=$(git rev-parse --show-toplevel)
 package_dir="$root/package"
 src_volume="$root":/go/src/github.com/davars/sohop
 
-docker run --rm -v $src_volume -v "$package_dir":/go/bin golang:1.9 sh -c 'CGO_ENABLED=0 go get -v github.com/davars/sohop/cmd/sohop'
+docker run --rm -v $src_volume -v "$package_dir":/go/bin golang:1.12 sh -c 'CGO_ENABLED=0 go get -v github.com/davars/sohop/cmd/sohop'
 docker build -t sohop-package "$package_dir"
 rm "$package_dir/sohop"
